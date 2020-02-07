798f43dd6777e815109a18bab748b1af73a45647
Alexandre Saudate <asaudate@uolinc.com>
2020-02-07 20:03:20 -0300

    removendo oauth2

--- cap07-resource-server/src/main/java/app/car/cap07/domain/PassengerRepository.java
+++
@@ -1,6 +0,0 @@
-package app.car.cap07.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PassengerRepository extends JpaRepository<Passenger, Long> {
-}
--- cap07-resource-server/src/main/java/app/car/cap07/domain/TravelRequestStatus.java
+++
@@ -1,5 +0,0 @@
-package app.car.cap07.domain;
-
-public enum TravelRequestStatus {
-    CREATED, ACCEPTED, REFUSED;
-}
