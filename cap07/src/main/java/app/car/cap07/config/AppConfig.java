81d6d4ab35dbc92b8a5e51cbd24c7265c735d971
Alexandre Saudate <asaudate@uolinc.com>
2020-02-17 09:01:35 -0300

    internacionalizacao

--- cap07/src/main/java/app/car/cap07/config/AppConfig.java
+++ cap07/src/main/java/app/car/cap07/config/AppConfig.java
@@ -3,6 +3,7 @@ package app.car.cap07.config;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.context.support.ResourceBundleMessageSource;
 
 @Configuration
@@ -11,8 +12,8 @@ public class AppConfig {
 
     @Bean
     public MessageSource messageSource() {
-        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
-        messageSource.setBasename("i18n/messages");
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:i18n/messages");
         messageSource.setDefaultEncoding("UTF-8");
         return messageSource;
     }
