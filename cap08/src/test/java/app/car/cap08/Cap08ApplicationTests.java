5676cfdbc32763074206226793d05b78209aa813
Alexandre Saudate <asaudate@uolinc.com>
2020-03-28 19:02:46 -0300

    testes faltando

---
+++ cap08/src/test/java/app/car/cap08/Cap08ApplicationTests.java
@@ -0,0 +1,13 @@
+package app.car.cap08;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cap08ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ cap09/src/test/java/app/car/cap09/Cap09ApplicationTests.java
@@ -0,0 +1,13 @@
+package app.car.cap09;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cap09ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
